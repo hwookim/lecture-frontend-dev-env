c911fb25e32d27798ce7e1614125ab217160330f
hwookim <gusdn9754@gmail.com>
2021-01-12 00:05:15 +0900

    feat: css, file 로더 적용

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,22 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.resolve('./dist'),
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        use: [
+          "file-loader",
+        ],
+      },
+    ],
+  },
 }
